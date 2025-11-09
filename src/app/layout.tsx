6a41540fc932d8b049bb140a31b79414024c27c4
nevisandegan <nevisandehgan@gmail.com>
2025-11-09 18:28:06 +0330

    feat(styles):Implelemntation fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,23 @@
 import "./globals.css";
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const bnazanin = localFont({
+  src: [
+    {
+      path: "../../public/fonts/B-NAZANIN.TTF",
+      style: "normal",
+    },
+  ],
+  variable: "--font-bnazanin",
+});
 
 export default function RootLayout({
   children,
@@ -6,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl">
+    <html
+      dir="rtl"
+      className={`${figtree.variable} ${bnazanin.variable}`}
+    >
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center">
           HEADER
         </header>
-          <div className="flex-1 flex">{children}</div>
+        <div className="flex-1 flex">{children}</div>
         <footer className="bg-gray-200 flex items-center justify-center">
           FOOTER
         </footer>
